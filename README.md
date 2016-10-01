3935996ae1c4311dff5b497e639e2c277aba9f53
YOUR FULL NAME <lun.mariancik@gmail.com>
2016-10-01 18:12:16 -0400

    updates

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 #Building Personal Portfolio Site
+//added 3 individual html files;
+// created css file;
+//made a image folder;
--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
-$(window).on('resize', function() {
-  'use strict';
-  var element	= document.querySelector('#banner'),
-    
-    })
+$(document).ready(function() {
+  $('.container').on('click', function() {
+  });
+})
+$.fn.extend({
+    animateCss: function (animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        this.addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+        });
+    }
+});
+$('body').animateCss('bounce');
+// $('.container').animateCss('zoomIn');
+$('.intro-text img').animateCss('rubberBand');
+$('.skills').animateCss('lightSpeedIn');
