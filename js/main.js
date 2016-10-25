1c4a74b4f16c480555ddbbc8e31c1b5354b58c73
YOUR FULL NAME <lun.mariancik@gmail.com>
2016-10-25 14:23:00 -0400

    updates

--- js/main.js
+++ js/main.js
@@ -9,7 +9,6 @@ $(window).on('resize', function() {
   wScrollBefore	= 0,
   wScrollDiff	= 0;
   window.addEventListener('scroll', function () {
-    // Hide the displayed menu. If you want to scroll, you're obviously not interested in the options.
     $('.navbar-collapse').collapse('hide');
     $('.navbar-toggle').addClass('collapsed').blur();
     if($(window).width() <= 768) {
@@ -20,44 +19,29 @@ $(window).on('resize', function() {
       wScrollDiff = wScrollBefore - wScrollCurrent;
       elTop = parseInt(window.getComputedStyle(element).getPropertyValue('top')) + wScrollDiff;
 
-      // scrolled to the very top; element sticks to the top
       if(wScrollCurrent <= 0) {
         element.style.top = '0px';
-      } // scrolled up; element slides in
+      }
       else if(wScrollDiff > 0) {
         element.style.top = (elTop > 0? 0 : elTop) + 'px';
-      } // scrolled down
+      }
       else if(wScrollDiff < 0) {
-        // scrolled to the very bottom; element slides in
         if(wScrollCurrent + wHeight >= dHeight - elHeight) {
           element.style.top = ( ( elTop = wScrollCurrent + wHeight - dHeight ) < 0 ? elTop : 0 ) + 'px';
-        } // scrolled down; element slides out
+        }
         else { element.style.top = ( Math.abs( elTop ) > elHeight ? -elHeight : elTop ) + 'px'; }
       }
       wScrollBefore = wScrollCurrent;
-    } //
+    } 
     else element.style.top = '0px';
   });
 }).resize();
 
-// Floating label headings for the contact form
-$(function() {$("body")
-.on("input propertychange", ".form-item",function(e) {
-  $(this).toggleClass("form-item-filled",!! $(e.target).val());})
-  .on("focus", ".form-item",function() {
-    $(this).addClass("form-item-focused");})
-    .on("blur", ".form-item",function() {
-      $(this).removeClass("form-item-focused");});
-    });
-
-    // Highlight the top nav as scrolling occurs
     $('body').scrollspy({target: '.navbar-fixed-top'})
 
-    // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a').click(function() {
       $(".navbar-collapse").collapse('hide');});
 
-      // jQuery for page scrolling feature - requires jQuery Easing plugin
       $(function() {$('body').on('click', 'a.scrollable', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({scrollTop: $($anchor.attr('href')).offset().top},1500,'easeInOutExpo');
@@ -67,7 +51,6 @@ $(function() {$("body")
     if (key === 27) $('.modal').modal('hide');
   })
 
-  // hide social icon containers when the icon is blocked by ad block
   if (!$('.fa-twitter').is(':visible')) {
     $('.fa-twitter').closest('li').css('display', 'none');
     $('.fa-linkedin').closest('li').css('display', 'none');
