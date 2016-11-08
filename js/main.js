385f49f2ff7ec199799d25578dd284c8e28681fd
YOUR FULL NAME <lun.mariancik@gmail.com>
2016-11-08 13:02:29 -0500

    updated home page

--- js/main.js
+++ js/main.js
@@ -16,3 +16,32 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+if ('ontouchstart' in window) { // detect touch
+  document.documentElement.className = document.documentElement.className +
+    'touch';
+}
+if (!$('html')
+  .hasClass('touch')) {
+
+  // background fix
+  $('.parallax')
+    .css('background-attachment', 'fixed');
+}
+
+// function to fix vertical when not overflow if .fullscreen content changes
+function fullscreenFix() {
+  var h = $('body')
+    .height();
+
+  // set .fullscreen height
+  $('.content-b')
+    .each(function (i) {
+      if ($(this)
+        .innerHeight() <= h) {
+        $(this)
+          .closest('.fullscreen')
+          .addClass('not-overflow');
+      }
+    });
+}
